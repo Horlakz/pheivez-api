c08a71afabd523146109fd84f58c62219fca5337
Horlakz <horlakz@proton.me>
2022-07-02 16:52:52 +0100

    added correct types to function

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,8 @@
+import type { Types } from "mongoose";
 import jwt from "jsonwebtoken";
 
 // generate token function
-const generateToken = (id: string) =>
+const generateToken = (id: Types.ObjectId) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });

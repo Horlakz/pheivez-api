460c98cacebc42d5c9317548799fdbb4ec0a416b
Horlakz <horlakz@proton.me>
2022-07-02 07:52:34 +0100

    added more fields to custom type

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,9 @@
 namespace NodeJS {
   interface ProcessEnv {
-    PORT: string;
+    NODE_ENV: string;
+    PORT: number;
     MONGO_URI: string;
+    JWT_SECRET: string;
+    JWT_EXPIRES_IN: string;
   }
 }

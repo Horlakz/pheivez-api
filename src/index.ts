cca80e39dc52c77eaaad416fe88f85067d4977f5
Horlakz <horlakz@proton.me>
2022-07-02 05:11:25 +0100

    added middleware to index file and add node_env development variable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 // all imports
 // import express and types
 import express, { Application } from "express";
-
+import cors from "cors";
+import logger from "morgan";
 import "dotenv/config";
-import middlewares from "./middlewares";
 import routes from "./routes/testRoute";
 import connectDB from "./config/db";
 
@@ -13,8 +13,22 @@ connectDB();
 // initialize express
 const app: Application = express();
 
-// middlewares
-middlewares.map((middleware) => app.use(middleware));
+// json parser
+app.use(express.json());
+
+// use middleware when in development
+if (process.env.NODE_ENV === "development") {
+  app.use(cors());
+  app.use(logger("dev"));
+}
+
+// cors
+app.use(
+  cors({
+    origin:
+      "http://localhost:4252, https://pheivez.netlify.app, https://pheivez.com",
+  })
+);
 
 // routes
 app.use("/api/v1/", routes);

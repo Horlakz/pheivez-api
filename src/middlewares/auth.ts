1cd968a5b2a3a957156b03500cdd427ec4b61f43
Horlakz <horlakz@proton.me>
2022-07-07 22:59:57 +0100

    created middle ware for protected routes

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,38 +1,54 @@
-import jwt from "jsonwebtoken";
+import { verify } from "jsonwebtoken";
 import type { Request, Response, NextFunction } from "express";
-import asyncHandler from "express-async-handler";
 import User from "../models/userModel";
 
-const protect = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    let token;
+interface JwtPayload {
+  id: string;
+}
 
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      token = req.headers.authorization.split(" ")[1];
-    }
+export const protect = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let token: string | undefined;
+  const authHeader: string | undefined = req.headers["authorization"];
 
-    if (!token) {
-      return res.status(401).json({
-        message: "Invalid Token",
-      });
-    }
-
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-
-      req.user = await User.findById(decoded.id).select("-password");
-
-      next();
-    } catch (err) {
-      res.status(401).json({
-        message: "Invalid Token",
-      });
-      process.exit(1);
-    }
+  if (authHeader === undefined || authHeader === null) {
+    return res.status(401).json({
+      message: "No token provided",
+    });
   }
-);
 
-export default protect;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  }
+
+  if (token === undefined || token === null) {
+    return res.status(401).json({
+      message: "Token is Missing",
+    });
+  }
+
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET!) as JwtPayload;
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (user === null) {
+      return res.status(401).json({
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err: any) {
+    res.status(401).json({
+      message: "Authentication Failed",
+      error: err.message,
+    });
+    process.exit(1);
+  }
+};

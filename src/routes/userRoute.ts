1e94e8e38ab1526732d85f4212367aa66b648bd2
Horlakz <horlakz@proton.me>
2022-07-07 23:00:17 +0100

    added protect middleware for get profile route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,13 +4,16 @@ import { Router } from "express";
 // get controllers
 import { register, login, getUser } from "../controllers/userController";
 
+// middlewares
+import { protect } from "../middlewares/auth";
+
 // initialize express router
 const router = Router();
 
 // create route
 router.post("/register", register);
 router.post("/login", login);
-router.get("/me", getUser);
+router.get("/me", protect, getUser);
 
 // export route(s)
 export default router;

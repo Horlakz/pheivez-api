f21947a381234ede4a860dee935141860c61bb62
Horlakz <horlakz@proton.me>
2022-07-08 08:21:39 +0100

    added send email function

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,40 @@
 import type { Types } from "mongoose";
 import { sign, Secret } from "jsonwebtoken";
+import nodemailer from "nodemailer";
+
+// types
+type HTMLElement = string | Buffer | undefined;
 
 // generate token function
-const generateToken = (id: Types.ObjectId) =>
+export const generateToken = (id: Types.ObjectId) =>
   sign({ id }, process.env.JWT_SECRET as Secret, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-// exports
-export { generateToken };
+// send email function
+export const sendEmail = async (
+  email: string,
+  subject: string,
+  html: HTMLElement
+) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions: nodemailer.SendMailOptions = {
+    from: `"Pheivez Arts" <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject,
+    html,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log(error);
+  }
+};

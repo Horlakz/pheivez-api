f4549495d31561a92051fcb5a3742b89d73d7db1
Horlakz <horlakz@proton.me>
2022-07-08 06:54:01 +0100

    added a describe function to test

--- src/tests/db.test.ts
+++ src/tests/db.test.ts
@@ -1,14 +1,16 @@
 import { connect, ConnectOptions } from "mongoose";
-import { it } from "vitest";
+import { describe, it } from "vitest";
 import "dotenv/config";
 
-it("it should connect to database", async () => {
-  const URI: string | undefined = process.env.MONGO_URI;
+describe("test database", () => {
+  it("it should connect to database", async () => {
+    const URI: string | undefined = process.env.MONGO_URI;
 
-  await connect(`${URI}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as ConnectOptions);
+    await connect(`${URI}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
 
-  console.log("Database connected successfully");
+    console.log("Database connected successfully");
+  });
 });

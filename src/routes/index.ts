7f09ef8755ce10004c14787f5e3685832c2c47c0
Horlakz <officialhorlakz@gmail.com>
2022-09-19 13:25:32 +0100

    updated code with art base route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { Router } from "express";
 // app routes
 import userRoutes from "./user";
 import newsLetterRoutes from "./newsletter";
+import artRoutes from "./art";
 
 // initialize express router
 const router = Router();
@@ -11,6 +12,7 @@ const router = Router();
 // mount routes
 router.use("/auth", userRoutes);
 router.use("/subscribers", newsLetterRoutes);
+router.use("/art", artRoutes);
 
 // export router
 export default router;

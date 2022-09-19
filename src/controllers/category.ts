2b9a5a44f101badbfd6e9828fc12782f5139348e
Horlakz <officialhorlakz@gmail.com>
2022-09-19 13:41:23 +0100

    created file for tag and category controller for basic crud

---
+++ src/controllers/category.ts
@@ -0,0 +1,56 @@
+import asyncHander from "express-async-handler";
+import type { Request, Response } from "express";
+import Category from "../models/category";
+
+// @desc    Get all categories
+// @route   GET /categories
+// @access  Public
+export const getCategories = asyncHander(
+  async (req: Request, res: Response) => {
+    try {
+      const categories = await Category.find();
+      res.status(200).json(categories);
+    } catch (err) {
+      if (err instanceof Error) res.status(400).json({ message: err.message });
+    }
+  }
+);
+
+// @desc    create category
+// @route   POST /categories
+// @access  Private
+export const createCategory = asyncHander(
+  async (req: Request, res: Response) => {
+    const { name } = req.body;
+
+    try {
+      // check if category exists
+      const checkCategory = await Category.findOne({ name });
+      if (checkCategory) {
+        res.status(400).json({ message: "Category already exists" });
+      }
+      const category = await Category.create({ name });
+      res.status(201).json(category);
+    } catch (err) {
+      if (err instanceof Error) res.status(400).json({ message: err.message });
+    }
+  }
+);
+
+// @desc delete category
+// @route DELETE /categories/:id
+// @access Private
+export const deleteCategory = asyncHander(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const category = await Category.findByIdAndDelete(id);
+      if (!category) {
+        res.status(404).json({ message: "Category not found" });
+      }
+      res.status(200).json({ message: "Category deleted" });
+    } catch (err) {
+      if (err instanceof Error) res.status(400).json({ message: err.message });
+    }
+  }
+);
---
+++ src/controllers/tag.ts
@@ -0,0 +1,52 @@
+import asyncHander from "express-async-handler";
+import type { Request, Response } from "express";
+import Tag from "../models/tag";
+
+// @desc    Get all tags
+// @route   GET /tags
+// @access  Public
+export const getCategories = asyncHander(
+  async (req: Request, res: Response) => {
+    try {
+      const tags = await Tag.find();
+      res.status(200).json(tags);
+    } catch (err) {
+      if (err instanceof Error) res.status(400).json({ message: err.message });
+    }
+  }
+);
+
+// @desc    create Tag
+// @route   POST /tags
+// @access  Private
+export const createTag = asyncHander(async (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  try {
+    // check if Tag exists
+    const checkTag = await Tag.findOne({ name });
+    if (checkTag) {
+      res.status(400).json({ message: "Tag already exists" });
+    }
+    const tag = await Tag.create({ name });
+    res.status(201).json(tag);
+  } catch (err) {
+    if (err instanceof Error) res.status(400).json({ message: err.message });
+  }
+});
+
+// @desc delete Tag
+// @route DELETE /tag/:id
+// @access Private
+export const deleteTag = asyncHander(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const tag = await Tag.findByIdAndDelete(id);
+    if (!tag) {
+      res.status(404).json({ message: "Tag not found" });
+    }
+    res.status(200).json({ message: "Tag deleted" });
+  } catch (err) {
+    if (err instanceof Error) res.status(400).json({ message: err.message });
+  }
+});

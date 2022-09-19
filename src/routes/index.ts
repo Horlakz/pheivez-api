a0867ca176ef3de09ce6ae4ccac6ba12c900f167
Horlakz <officialhorlakz@gmail.com>
2022-09-19 11:06:23 +0100

    imported newsletterRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,14 @@ import { Router } from "express";
 
 // app routes
 import userRoutes from "./user";
+import newsLetterRoutes from "./newsletter";
 
 // initialize express router
 const router = Router();
 
 // mount routes
 router.use("/auth", userRoutes);
+router.use("/subscribers", newsLetterRoutes);
 
 // export router
 export default router;

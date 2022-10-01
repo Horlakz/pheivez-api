5cda7a2bbbe2b075b10c70b79cbecfc3cc6aea1f
Horlakz <officialhorlakz@gmail.com>
2022-10-01 14:01:49 +0100

    removes pip function and stream file function from route

--- src/routes/media.ts
+++ src/routes/media.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
-import { getFile } from "../config/media";
+import { getFile } from "../controllers/media";
 
 const router = Router();
 
-router.get("/:key", (req, res) => getFile(req.params.key).pipe(res));
+router.get("/:key", getFile);
 
 export default router;

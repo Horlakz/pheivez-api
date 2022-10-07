50dc783665052a0d156f1af348270ae770b8c1e2
Horlakz <officialhorlakz@gmail.com>
2022-10-07 19:29:35 +0100

    updates key path

--- src/controllers/media.ts
+++ src/controllers/media.ts
@@ -4,7 +4,7 @@ import { pipeline } from "stream/promises";
 
 export const getFile = async (req: Request, res: Response) => {
   const { key } = req.params;
-  const readStream = streamFile(key);
+  const readStream = streamFile(`projects/pheivez/${key}`);
 
   res.setHeader("Content-Type", "image/jpeg");
  try {
